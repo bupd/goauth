ec6ab1ba5c99e6d9bbf45d21d9831a3fe2517a77
bupd <bupdprasanth@gmail.com>
2024-05-12 06:18:01 +0530

    update: authentication basic func

--- main.go
+++ main.go
@@ -3,12 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/bupd/goauth/users"
 )
 
 func main() {
 	fmt.Printf("kumar")
 	http.HandleFunc("/", userHandler)
 	http.ListenAndServe("", nil)
+}
+
+func getSignInPage(w http.ResponseWriter, r *http.Request) {
+}
+
+func getSignUpPage(w http.ResponseWriter, r *http.Request) {
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+}
+
+func getUser(r *http.Request) users.User {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	return users.User{
+		Email:    email,
+		Password: password,
+	}
 }
 
 func userHandler(w http.ResponseWriter, r *http.Request) {
